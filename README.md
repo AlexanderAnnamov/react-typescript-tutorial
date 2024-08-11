ade9b68de6e2616d9cff9fee35d03cd297f899cf
Александр Аннамов <hash_nam@mail.ru>
2024-08-11 23:48:30 +0500

    polymorphic Components

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This template provides a minimal setup to get React working in Vite with HMR and
 
 Currently, two official plugins are available:
 
+https://www.youtube.com/playlist?list=PLC3y8-rFHvwi1AXijGTKM0BKtHzVC-LSK
+
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
--- src/types-for/props.tsx
+++ src/types-for/props.tsx
@@ -117,3 +117,19 @@ export const ToastNew = (props: ComponentProps<typeof Toast>) => {
         </div>
     )
 }
+
+// Polymorphic Components
+
+type TextOwnProps<E extends React.ElementType> = {
+    size?: 'sm' | 'md' | 'lg';
+    color?: 'primary' | 'secondary';
+    as?: E;
+    children: ReactNode
+}
+
+type TextProps<E extends React.ElementType> = TextOwnProps<E> & Omit<React.ComponentProps<E>, keyof TextOwnProps<E>>
+
+export const Text = <E extends React.ElementType = 'div'> ({as, size, color, children}: TextProps<E>) => {
+    const Component = as || 'div';
+    return <Component className={`class-with-${color}-${size}`}>{children}</Component>
+}
